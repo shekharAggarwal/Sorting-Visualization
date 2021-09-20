2b852fc1673d4a29ddd07f02f87b7af072f09b50
shekharAggarwal <aggarwalshekhar99@gmail.com>
2021-09-20 10:46:39 +0530

    initial commit

---
+++ lib/presentation/journeys/selectSorting/select_sorting.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SelectSorting extends StatefulWidget {
+  const SelectSorting({Key? key}) : super(key: key);
+
+  @override
+  _SelectSortingState createState() => _SelectSortingState();
+}
+
+class _SelectSortingState extends State<SelectSorting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(),
+    );
+  }
+}
